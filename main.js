324afb5d2590da4d1597be6f17c8302c7c8f1dca
Pavel <bonyown2012@gmail.com>
2019-01-10 17:20:56 +0300

    test some tricks

---
+++ main.js
@@ -0,0 +1,9 @@
+alert( "Привет" );
+console.log('1');
+
+var bodyColor = document.querySelector('div');
+
+bodyColor.onclick = function(){
+    bodyColor.style.backgroundColor = 'white';
+    bodyColor.style.height = '200px';
+}
