df19f81039c71e1bad83311eedc3608e70b54a33
Pavel <bonyown2012@gmail.com>
2019-01-10 19:35:56 +0300

    calc rdy)

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-alert( "Привет" );
-console.log('1');
+// alert( "Привет" );
+
 
 var bodyColor = document.querySelector('div');
 
+// btn's
+var plus = document.querySelector('#plus');
+var minus = document.querySelector('#minus');
+var multiply = document.querySelector('#multiply');
+var division = document.querySelector('#division');
+var resBlock = document.querySelector('#result');
+
+// numbers
+var firstNum = document.querySelector('#firstNum');
+var secondNum = document.querySelector('#secondNum');
+
+
+// some functions
 bodyColor.onclick = function(){
     bodyColor.style.backgroundColor = 'white';
-    bodyColor.style.height = '200px';
 }
+
+
+// math functions
+plus.onclick = function(){
+    var result = parseInt(firstNum.value) + parseInt(secondNum.value);
+    resBlock.innerHTML = 'Итого: ' + result;
+}
+
+minus.onclick = function(){
+    var result = parseInt(firstNum.value) - parseInt(secondNum.value);
+    resBlock.innerHTML = 'Итого: ' + result;
+}
+
+multiply.onclick = function(){
+    var result = parseInt(firstNum.value) * parseInt(secondNum.value);
+    resBlock.innerHTML = 'Итого: ' + result;
+}
+
+division.onclick = function(){
+    var result = parseInt(firstNum.value) / parseInt(secondNum.value);
+    resBlock.innerHTML = 'Итого: ' + result;
+}
+
