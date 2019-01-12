27c74a921ed572eded68ee21525e7b240ec7baa4
Pavel <bonyown2012@gmail.com>
2019-01-12 16:53:25 +0300

    Human turn's

--- main.js
+++ main.js
@@ -34,15 +34,47 @@ function startGame() {
 //получаем клик игрока
 function turnClick(square) {
     turn(square.target.id, huPlayer);
-    // console.log(square.target.id);
+    //  console.log(square.target.id);
 }
 
 function turn(squareId, player) {
     origBoard[squareId] = player;
     document.getElementById(squareId).innerText = player;
-    
+    let gameWon = checkWin(origBoard, player);
+    if (gameWon) gameOver(gameWon);
 }
 
 
 // 20:14
 // https://www.youtube.com/watch?v=P2TcQ3h0ipQ&index=6&list=PLWKjhJtqVAbleDe3_ZA8h3AO2rXar-q2V
+
+
+
+// cheak win
+
+function checkWin(board, player) {
+    let plays = board.reduce((a, e, i) =>
+        (e === player) ? a.concat(i) : a, []);
+    let gameWon = null;
+    for (let [index, win] of winCombos.entries()) {
+        if (win.every(elem => plays.indexOf(elem) > -1)) {
+            gameWon = { index: index, player: player };
+            break;
+        }
+    }
+    return gameWon;
+}
+
+// game over f
+function gameOver(gameWon) {
+    for (let index of winCombos[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor =
+            gameWon.player == huPlayer ? "green" : "red";
+    }
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', turnClick, false);
+    }
+}
+
+
+
